5d2048d60cee63df96820e8642fae81e8090747d
aaditya-shrivastava <aadityashrivastava2004@gmail.com>
2026-05-29 11:04:00 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mp = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        stack = []
+        for ch in s:
+            if ch in mp.values():
+                stack.append(ch)
+            else:
+                if not stack or stack[-1] != mp[ch]:
+                    return False
+                stack.pop()
+        return len(stack) == 0
