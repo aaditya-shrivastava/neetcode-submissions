ce115e9a70cadb1ffb736f125eaebcb587a98498
aaditya-shrivastava <aadityashrivastava2004@gmail.com>
2026-06-02 23:51:16 +0530

    Refactor findMedianSortedArrays method
    Removed unused variables and simplified the code.

--- Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.py
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.py
@@ -2,7 +2,6 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         num = nums1 + nums2
         nums = sorted(num)
-        l, r = len(nums1), len(nums2)
         mid = len(nums) // 2
         ans = 0
         if len(nums) % 2 != 0:
